ae5a157bf06336cf3df4de7c6626426e80dc8907
Om Thanage <om.thanage@somaiya.edu>
2024-12-13 13:52:36 +0530

    Refactor: About Us page Add team page

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -1,16 +1,25 @@
 function AboutUs() {
   return (
-    <div className='flex flex-col justify-start items-center dark:bg-black bg-white p-8 mt-28'>
-        <h1 className='text-4xl sm:text-6xl uppercase font-black dark:text-white text-black mb-4 md:mb-8 md:text-8xl '>About Us</h1>
-        <div className='flex flex-col md:flex-row justify-between md:items-start mb-16 w-full h-1/2'>
-            <h2 className='text-2xl text-center md:text-left sm:text-4xl md:text-5xl lg:text-6xl dark:text-white text-black mb-4 md:mb-0 md:mr-8 font-bold md:pb-4'>Together we STRIVE,<br />together we ACHIEVE</h2>
+    <div className='flex flex-col justify-center items-center bg-gradient-to-b from-indigo-950 via-cyan-900  to-cyan-400  p-8 mt-28 bg-opacity-90'>
+        <h1 className='text-4xl uppercase font-black dark:text-white text-black px-8 mb-4 md:mb-8 border-t-cyan-400 border-t-2'>About Us</h1>
+        <div className='flex flex-col md:flex-row justify-center md:items-center mb-16 w-full h-1/2'>
+          <div className='flex flex-col justify-center items-center md:w-1/2 w-full'>     
+            <div className='md:text-6xl text-3xl text-center dark:text-white text-black mb-4 md:mb-0 md:mr-8 font-bold md:pb-4'>
+              Together <span className="text-cyan-400">We</span> Strive,
+            </div>
+            <div className='md:text-6xl text-3xl text-center dark:text-white text-black mb-4 md:mb-0 md:mr-8 font-bold md:pb-4'>
+              Together <span className="text-cyan-400">We</span> Achieve!
+            </div>
+          </div>
             <p className='text-1xl lg:text-2xl text-white md:w-1/2 text-justify'>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ultricies malesuada nisi commodo consectetur. Fusce sed tortor finibus risus efficitur dignissim a nec nisi. Nam consectetur consectetur urna. Nam egestas pulvinar nulla eu finibus. Ut interdum finibus justo eget lacinia. Etiam aliquet fermentum sem sed pellentesque. Sed fermentum convallis elit.
+            Welcome to KJSCE ACM, where caffeine fuels ideas, bugs are just happy accidents, and tech dreams take shape! 
+We're the cool new kids on campus. From cracking code to cracking jokes, we're all about learning, growing, and making things happen.
+At KJSCE ACM, our mantra is simple: together we strive, together we achieve..... and maybe have a little too much fun along the way!
             </p>
         </div>
         <div className='relative flex justify-center'>
-            <div className='absolute inset-0 transform  translate-x-1/4 md:translate-x-1/3 -translate-y-4 bg-gradient-to-r from-purple-500 to-pink-500 md:w-2/3 w-4/5 rounded-2xl'></div>
-            <img src='https://picsum.photos/1600/900' alt='About Us' className='relative z-10 md:w-2/3 w-4/5 rounded-2xl md:mt-12 mt-0' />
+            <div className='absolute inset-0 transform  -translate-x-1/12 md:translate-x-1/3 -translate-y-4 bg-gradient-to-r from-purple-500 to-pink-500 md:w-2/3 w-11/12 rounded-2xl'></div>
+            <img src='team.jpg' alt='About Us' className='relative z-10 md:w-2/3 w-11/12 rounded-2xl md:mt-12 mt-0' />
         </div>
     </div>
   )
--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -1,18 +1,35 @@
-import React from 'react'
+import React from 'react';
+
+const teamSections = [
+  { position: 'CORE', style: 'bg-indigo-900 z-20', size: 'h-[450px]' },
+  { position: 'TECH', style: 'bg-red-500 top-6 left-[300px]', size: 'h-[300px]' },
+  { position: 'CRT', style: 'bg-blue-500 top-6 right-[300px]', size: 'h-[300px]' },
+  { position: 'MRT', style: 'bg-yellow-500 bottom-6 left-[150px]', size: 'h-[300px]' },
+  { position: 'PR', style: 'bg-green-500 bottom-6 right-[150px]', size: 'h-[300px]' },
+  { position: 'OPS', style: 'bg-blue-500', size: 'h-[300px]' },
+];
 
 function Team() {
   return (
-    <div id = "our-team" className='flex flex-col justify-start items-center dark:bg-black bg-white p-8'>
-      <div className='text-4xl sm:text-6xl uppercase font-black dark:text-white text-black mb-4 md:mb-8 md:text-8xl '>Our Team</div>
-      <div className='grid grid-cols-3 gap-4'>
-        <img src='https://picsum.photos/300/400' alt='Team' className='w-full h-auto' />
-        <img src='https://picsum.photos/300/400' alt='Team' className='w-full h-auto' />
-        <img src='https://picsum.photos/300/400' alt='Team' className='relative w-full h-auto top-4' />
-        <img src='https://picsum.photos/300/400' alt='Team' className='w-full h-auto' />
-        <img src='https://picsum.photos/300/400' alt='Team' className='w-full h-auto' />
+    <div
+      id="our-team"
+      className="flex flex-col justify-center items-center bg-gradient-to-b from-indigo-950 via-cyan-900 to-cyan-400 p-8"
+    >
+      <div className="text-4xl uppercase font-black dark:text-white text-black px-8 mb-4 md:mb-16 border-t-cyan-400 border-t-2">
+        Our Team
+      </div>
+      <div className="relative w-full h-[450px] flex justify-center items-center text-4xl mb-16">
+        {teamSections.map((section, index) => (
+          <div
+            key={index}
+            className={`absolute ${section.style} text-white font-bold flex justify-center items-center w-1/3 max-w-[450px] ${section.size} rounded-xl`}
+          >
+            {section.position}
+          </div>
+        ))}
       </div>
     </div>
-  )
+  );
 }
 
-export default Team
+export default Team;
