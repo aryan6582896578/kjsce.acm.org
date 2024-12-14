b10f65a5555a6e20b24f11788ada7438d59317b6
Om Thanage <om.thanage@somaiya.edu>
2024-12-15 00:51:26 +0530

    Feature: Add globe component

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import AboutUs from './components/AboutUs'
 import Team from './components/Team'
 import Footer from './components/Footer'
 import "./App.css"
+import { Earth } from './components/Earth'
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <div className="w-full h-full overflow-hidden">
         <div className=' container mx-auto'>
           <Navbar />
+          <Earth />
           <AboutUs />
           <Team />
           <Footer/>
