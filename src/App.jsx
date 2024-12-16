255beb81e356f9f49afa84e34a1a6bc6e37b294c
Ashutosh R Dosi <ashutoshdosi@gmail.com>
2024-12-16 16:45:43 +0530

    Add AboutUs Section

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import AboutUs from './components/AboutUs'
 import Team from './components/Team'
-// import ContactUs from './components/ContactUs'
+import ContactUs from './components/ContactUs'
 import Footer from './components/Footer'
 import "./App.css"
 
@@ -15,8 +15,8 @@ function App() {
           <Navbar />
           <Hero/>
           <AboutUs />
-          {/* <ContactUs/> */}
           <Team />
+          <ContactUs/>
           <Footer/>
           {/* <div className="h-64 w-20 bg-blue-600 blur-[90px] absolute top-64 mt-96 left-0"></div> div for blur blue */}
 
---
+++ src/components/SocialLink.jsx
@@ -0,0 +1,34 @@
+import * as React from "react";
+
+
+
+export default function SocialLink() {
+  return (
+    <>
+    <div className="flex gap-6 items-center">
+      <img
+        src=""
+        className="object-contain shrink-0 my-auto aspect-square w-[32px]"
+        alt="whatsapp icon"
+      />
+      <div className="text-xl text-slate-300"><h3>placeholder for whatsapp</h3></div>
+    </div>
+    <div className="flex gap-6 items-center">
+      <img
+        src=""
+        className="object-contain shrink-0 my-auto aspect-square w-[32px]"
+        alt="linkedin icon"
+      />
+      <div className="text-xl text-slate-300"><h3>placeholder for linkedin</h3></div>
+    </div>
+    <div className="flex gap-6 items-center">
+      <img
+        src=""
+        className="object-contain shrink-0 my-auto aspect-square w-[32px]"
+        alt="whatsapp icon"
+      />
+      <div className="text-xl text-slate-300"><h3>@kjsce_acm</h3></div>
+    </div>
+    </>
+  );
+}
